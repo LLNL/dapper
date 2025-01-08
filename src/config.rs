70ce51191337b79d231ef5c8decfb58bc2b55899
Micaela <gallegos31@llnl.gov>
2025-01-08 14:42:49 -0800

    Add file with function to return the path of the standard location.

---
+++ src/config.rs
@@ -0,0 +1,13 @@
+// Copyright 2024 Lawrence Livermore National Security, LLC
+// See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+use directories::BaseDirs;
+
+pub fn get_base_directory() -> &Path
+{
+    if let Some(base_dirs) = BaseDirs::new() {
+        base_dirs.data_dir();
+    }
+}
