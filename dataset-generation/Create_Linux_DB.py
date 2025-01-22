33ee2d1a3a566b7fb0ea8df88c7340fe7c07404a
Steven West <west50@llnl.gov>
2025-01-21 23:47:46 -0800

    Updated Linux database creation script; Added normalized filename column

--- dataset-generation/Create_Linux_DB.py
+++ dataset-generation/Create_Linux_DB.py
@@ -6,11 +6,17 @@ The result is stored in a sqlite database
 
 The database has one table:
     "package_files"
-The table has four main columns:
-    file_name           - Just the name of the file         ex: "lib/modules/5.4.0-1009-aws/vdso/vdso32.so" -> vdso32.so
-    file_path           - The entire path for the file      ex: "lib/modules/5.4.0-1009-aws/vdso/vdso32.so"
-    package_name        - The short package name            ex: "admin/multipath-tools" -> multipath-tools
-    full_package_name   - The full/long package name        ex: admin/multipath-tools
+The table has five main columns:
+    file_name               - Just the name of the file
+                              ex: "lib/modules/5.4.0-1009-aws/vdso/vdso32.so" -> vdso32.so
+    normalized_file_name    - The filename normalized to remove version info
+                              ex: "libexample-1.2.3.so" -> "libexample.so"
+    file_path               - The entire path for the file
+                              ex: "lib/modules/5.4.0-1009-aws/vdso/vdso32.so"
+    package_name            - The short package name
+                              ex: "admin/multipath-tools" -> multipath-tools
+    full_package_name       - The full/long package name
+                              ex: admin/multipath-tools
 
 The file_name column is indexed for fast lookups, as the file_name is the primary value that will be searched for
 """
@@ -29,6 +35,8 @@ from urllib.parse import urlparse
 from tqdm.auto import tqdm
 
 from typing_extensions import Self
+
+from dapper_python.normalize import NormalizedFileName, normalize_file_name
 
 
 @dataclass
@@ -82,18 +90,36 @@ def read_data(uri:str|Path, *, encoding='utf-8') -> TextIOWrapper:
         if not (parsed_url.scheme and parsed_url.netloc):
             raise ValueError(f"Invalid URL: {uri}")
 
-        #Data is most commonly in a compressed gzip format, but support some others as well
-        with requests.get(uri) as web_request:
+        with requests.get(uri, stream=True) as web_request:
+            if 'content-length' in web_request.headers:
+                file_size = int(web_request.headers['content-length'])
+            else:
+                file_size = None
+
+            content = BytesIO()
+            progress_bar = tqdm(
+                    total=file_size,
+                    desc='Downloading package file', colour='blue',
+                    unit='B', unit_divisor=1024, unit_scale=True,
+                    position=None, leave=None,
+                )
+            with progress_bar:
+                for chunk in web_request.iter_content(chunk_size=8*1024):
+                    content.write(chunk)
+                    progress_bar.update(len(chunk))
+            content.seek(0)
+
+            #Data is most commonly in a compressed gzip format, but support some others as well
             match web_request.headers.get('Content-Type', None):
                 case 'application/x-gzip':
-                    with gzip.open(BytesIO(web_request.content)) as gz_file:
+                    with gzip.open(content) as gz_file:
                         return TextIOWrapper(BytesIO(gz_file.read()), encoding=encoding)
                 case 'application/x-xz':
-                    with lzma.open(BytesIO(web_request.content)) as lzma_file:
+                    with lzma.open(content) as lzma_file:
                         return TextIOWrapper(BytesIO(lzma_file.read()), encoding=encoding)
                 case _:
                     #Not sure, try to read as raw text file
-                    return TextIOWrapper(BytesIO(web_request.content))
+                    return TextIOWrapper(content)
 
     else:
         raise TypeError(f"Invalid input: {uri}")
@@ -118,23 +144,26 @@ def main():
     )
     args = parser.parse_args()
 
+    #Currently not set up to be able to handle resuming a previously started database
+    #However it's not a high priority as the process only takes a minute or two. Can just delete the old DB and recreate
+    #TODO: Potentially allow resuming in the future
+    if args.output.exists():
+        raise FileExistsError(f"File {args.output} already exists")
+
     file = read_data(args.input)
     line_count = sum(1 for _ in file)
     file.seek(0)
 
-    #Currently not set up to be ble to handle resuming a previously started database
-    #However it's not a high priority as the process only takes a minute or two. Can just delete the old DB and recreate
-    #TODO: Allow resuming in the future
-    if args.output.exists():
-        raise FileExistsError(f"File {args.output} already exists")
-
     with sqlite3.connect(args.output) as db:
         cursor = db.cursor()
 
+        #Would there be any benefit to having a separate package table
+        #Which the files table references as a foreign key vs directly saving the package into the files table?
         create_table_cmd = """
             CREATE TABLE package_files(
                 id INTEGER PRIMARY KEY,
                 file_name TEXT,
+                normalized_file_name TEXT,
                 file_path TEXT,
                 package_name TEXT,
                 full_package_name TEXT
@@ -143,8 +172,8 @@ def main():
         cursor.execute(create_table_cmd)
 
         insert_cmd = """
-            INSERT INTO package_files(file_name, file_path, package_name, full_package_name)
-            VALUES (?, ?, ?, ?)
+            INSERT INTO package_files(file_name, normalized_file_name, file_path, package_name, full_package_name)
+            VALUES (?, ?, ?, ?, ?)
         """
         progress_iter = tqdm(
             file,
@@ -154,12 +183,23 @@ def main():
         )
         for line in progress_iter:
             package = PackageInfo.from_linux_package_file(line)
+
+            #Lower seems like it should work? As far as the OS is concerned ß.json is not the same file as ss.json
+            normalized_file = normalize_file_name(package.file_name)
+            match normalized_file:
+                case str(name):
+                    normalized_name = name.lower()
+                case NormalizedFileName():
+                    normalized_name = normalized_file.name.lower()
+                case _:
+                    raise TypeError(f"Failed to normalize file: {package.file_name}")
+
             cursor.execute(
                 insert_cmd,
                 (
-                    package.file_name, str(package.file_path),
+                    package.file_name, normalized_name, str(package.file_path),
                     package.package_name, package.full_package_name,
-                 )
+                )
             )
 
         #Index the filename colum for fast lookups
@@ -167,6 +207,11 @@ def main():
         index_cmd = """
             CREATE INDEX idx_file_name
             ON package_files(file_name);
+        """
+        cursor.execute(index_cmd)
+        index_cmd = """
+            CREATE INDEX idx_normalized_file_name
+            ON package_files(normalized_file_name);
         """
         cursor.execute(index_cmd)
 
