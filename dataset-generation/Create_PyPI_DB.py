b97657f5fd9656928f36e07a45a3c6e7fd4ae964
Steven West <west50@llnl.gov>
2025-04-30 16:32:18 -0700

    Update PyPI database to save file list + type information from package wheel

--- dataset-generation/Create_PyPI_DB.py
+++ dataset-generation/Create_PyPI_DB.py
@@ -22,18 +22,26 @@ from __future__ import annotations
 import argparse
 import requests
 import sqlite3
+import zipfile, zlib
 import math
 import time
 import io
+import more_itertools
 import concurrent.futures
-import zipfile
 import functools
+import methodtools
 
-from dataclasses import dataclass
+#On Windows/Mac: Install "python-magic-bin" from pip to also get executable, don't install python-magic, python-libmagic, etc
+#On Linux, this doesn't work because it doesn't have linux executables
+#But can potentially just install python-magic which will use the underlying linux utilities
+import magic
+
+from dataclasses import dataclass, field
 from pathlib import Path, PurePosixPath
 from http import HTTPStatus
 from contextlib import suppress
 from natsort import natsorted
+from zipfile import ZipFile
 from tqdm.auto import tqdm
 from itertools import repeat
 from more_itertools import chunked
@@ -42,6 +50,35 @@ from typing import Final, ClassVar, Literal
 from typing import TypeVar, Callable, ParamSpec
 from typing import Generator, Iterable, Any
 from typing_extensions import Self
+
+from dapper_python.normalize import normalize_file_name
+
+
+@dataclass
+class PackageDetails:
+    """Container class for storing information about a package that we want to save to the database
+
+    Acts as a convent single-object to return from processing function to make calling via concurrent.futures cleaner
+    """
+    name: str
+    serial: int|None
+
+    version: str|None = None
+    imports: set[str] = field(default_factory=set)
+    files: set[FileDetails] = field(default_factory=set)
+
+@dataclass(frozen=True)
+class FileDetails:
+    """Details about an individual file
+
+    Includes extra information about the type of the file which may not be obvious just from its extension
+    mime_type is the result of asking libmagic for the mime type
+    and magic_string is the full-length description that libmagic returns
+    """
+    file: PurePosixPath
+    mime_type: str|None = field(default=None, compare=False, hash=False)
+    magic_string: str|None = field(default=None, compare=False, hash=False)
+
 
 
 T = TypeVar("T")
@@ -152,7 +189,7 @@ class PyPIDatabase:
             """
             cursor.execute(create_index_cmd)
 
-            #User-facing view which hides the backend tracking logic
+            #User-facing view for imports which hides the backend tracking logic
             create_view_cmd = """
                 CREATE VIEW
                 IF NOT EXISTS v_package_imports
@@ -161,6 +198,33 @@ class PyPIDatabase:
                     FROM packages
                     JOIN package_imports
                     ON packages.id = package_imports.package_id
+            """
+            cursor.execute(create_view_cmd)
+
+            create_table_cmd = """
+            CREATE TABLE
+            IF NOT EXISTS package_files(
+                id INTEGER PRIMARY KEY,
+                package_id INTEGER,
+                file_name TEXT,
+                normalized_file_name TEXT,
+                file_path TEXT,
+                mime_type TEXT,
+                magic_string TEXT,
+                FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+            )
+            """
+            cursor.execute(create_table_cmd)
+
+            #User-facing view for files which hides the backend tracking logic
+            create_view_cmd = """
+                CREATE VIEW
+                IF NOT EXISTS v_package_files
+                AS 
+                    SELECT package_name, normalized_file_name, file_name, file_path, mime_type, magic_string
+                    FROM packages
+                    JOIN package_files
+                    ON packages.id = package_files.package_id
             """
             cursor.execute(create_view_cmd)
 
@@ -194,7 +258,7 @@ class PyPIDatabase:
         yield from packages
 
     @_requires_connection
-    def add_package_imports(self, package_name:str, package_imports:str|Iterable[str], *, serial:int) -> None:
+    def add_package(self, package_details:PackageDetails) -> None:
         """Adds a package and import names to the database
 
         :param package_name: Name of the package
@@ -202,29 +266,44 @@ class PyPIDatabase:
         :param serial: A number designed to keep track of when the package was processed
                        Intended to be the last_serial field from the PyPI index, but could be a custom value
         """
-        if isinstance(package_imports, str):
-            package_imports = (package_imports, )
+        if not package_details.imports and not package_details.files:
+            #Nothing useful to add to the database
+            return
 
         with self.get_cursor() as cursor:
             insert_package_cmd = """
                 INSERT INTO packages(package_name, last_serial)
                 values (?, ?)
             """
-            cursor.execute(insert_package_cmd, (package_name, serial))
+            cursor.execute(insert_package_cmd, (package_details.name, package_details.serial))
 
             #We need the saved id in order to reference as foreign key in the package_imports table
             package_id = cursor.lastrowid
 
-            insert_import_cmd = """
-                INSERT INTO package_imports(package_id, import_as)
-                values (?, ?)
-            """
-            cursor.executemany(insert_import_cmd, zip(repeat(package_id), package_imports))
+            if package_details.imports:
+                insert_import_cmd = """
+                    INSERT INTO package_imports(package_id, import_as)
+                    values (?, ?)
+                """
+                cursor.executemany(insert_import_cmd, zip(repeat(package_id), package_details.imports))
+
+            if package_details.files:
+                insert_file_cmd = """
+                    INSERT INTO package_files(package_id, file_name, normalized_file_name, file_path, mime_type, magic_string)
+                    values (?, ?, ?, ?, ?, ?)
+                """
+                data = (
+                    (package_id, file.file.name, str(normalize_file_name(file.file.name)), str(file.file), file.mime_type, file.magic_string)
+                    for file in package_details.files
+                )
+                cursor.executemany(insert_file_cmd, data)
 
     @_requires_connection
     def remove_packages(self, package_names:str|Iterable[str]) -> None:
         """Removes the specified package(s) from the database
-        This will remove the package from the "packages" table and also associated entries in "package_imports" table
+
+        This will remove the package from the "packages" table
+        Along with associated entries in "package_imports" and "package_files" tables
 
         :param package_names: Name(s) of the package(s) to remove
         """
@@ -244,8 +323,8 @@ class PyPIDatabase:
 @dataclass
 class PyPIPackage:
     package_name: str
-    last_serial: int|None = None
 
+    @methodtools.lru_cache(maxsize=1)
     def get_package_info(self) -> dict[str, Any]:
         """Gets the information contained on the package's PyPI page in json format
 
@@ -254,26 +333,16 @@ class PyPIPackage:
         url = self._API_PACKAGE_URL.format(package_name=self.package_name)
         return self._web_request(url).json()
 
-
-    def get_imports(self) -> list[str]|None:
-        """Downloads the .whl (wheel) file for the package and attempts to determine how the package is imported
-        This may differ from the name of the package itself
-        eg: BeautifulSoup4 is installed as "pip install beautifulsoup4" but used in source code as "import bs4"
-
-        :return: A list of names that can be imported from the package
-                 If unable to determine, then None is returned instead
-        """
+    def _get_wheel_files(self) -> Generator[ZipFile, None, None]:
         package_info = self.get_package_info()
 
-        releases = package_info['releases']
-        releases = dict(natsorted(releases.items(), reverse=True))
-
         #Only keep ones that have wheels and have not been yanked
+        releases = dict(natsorted(package_info['releases'].items(), reverse=True))
         releases = {
             version:data
             for version, data in releases.items()
             if any(
-                x['url'].endswith('.whl')
+                x['packagetype'] == 'bdist_wheel'
                 and not x['yanked']
                 for x in data
             )
@@ -281,66 +350,110 @@ class PyPIPackage:
         if not releases:
             return None
 
+        #Grab all wheels (for all architectures) from the latest version that has not been yanked and has some wheels
         version, release_data = next(iter(releases.items()))
-        release_data = next(
-            x
-            for x in release_data
-            if x['url'].endswith('.whl')
-            and not x['yanked']
-        )
+        for entry in release_data:
+            if not entry['packagetype'] == 'bdist_wheel':
+                continue
 
-        with self._web_request(release_data['url']) as web_request:
-            data = io.BytesIO(web_request.content)
+            with self._web_request(entry['url'], stream=True) as web_request:
+                data = io.BytesIO(web_request.content)
+            with suppress(zipfile.BadZipFile):
+                yield ZipFile(data)
 
-        with zipfile.ZipFile(data) as wheel_file:
-            files = tuple(PurePosixPath(x) for x in wheel_file.namelist())
+    def _get_imports(self, wheel_file:ZipFile) -> set[str]:
+        imports = set()
+        package_files = list(PurePosixPath(x) for x in wheel_file.namelist())
 
-            #Sometimes contains a top_level.txt file which details the top-level imports for the package
-            #If this is available, then use it as it's likely to be the most reliable information
-            top_level_txt = next((x for x in files if x.name == 'top_level.txt'), None)
-            if top_level_txt:
-                text_data = wheel_file.read(str(top_level_txt)).decode('utf-8')
-                imports = list(line.strip() for line in text_data.splitlines() if line)
-                #Sometimes these files can be empty
-                if imports:
-                    #TODO: If a top_level.txt is present, does this preclude other imports?
-                    #I.e. Should we keep checking or stop here?
-                    return imports
+        #Sometimes contains a top_level.txt file which details the top-level imports for the package
+        #If this is available, then use it as it's likely to be the most reliable information
+        top_level_txt = next((x for x in package_files if x.name == 'top_level.txt'), None)
+        if top_level_txt:
+            text_data = wheel_file.read(str(top_level_txt)).decode('utf-8')
+            imports = set(line.strip() for line in text_data.splitlines() if line)
+            imports.update(imports)
 
-            #If it doesn't have that file or couldn't parse it
-            #Then fall back on trying to check what directories are importable as modules
-            top_level_paths = tuple({
-                PurePosixPath(x.parents[-2] if len(x.parents) >= 2 else x)
-                for x in files
-            })
+            #Sometimes these files can be empty
+            if imports:
+                #TODO: If a top_level.txt is present, does this preclude other imports?
+                #I.e. Should we keep checking or stop here?
+                return imports
 
-            #Check for any top-level python files, as these should also be importable
-            importable_files = {
-                file.stem
-                for file in top_level_paths
-                if file.name.endswith('.py')
-                and not file.name.startswith('_')
-            }
+        #TODO: Any other/better methods for determining importable names?
+        #This seems to produce a fair amount of correct values, but also a fair number of duplicates across packages
 
-            #Check for any top-level paths that contain an __init__.py
-            importable_dirs = {
-                directory.name
-                for directory in (Path(y) for y in top_level_paths)
-                if any(
-                    file.name == '__init__.py'
-                    and file.parent == directory
-                    for file in files
-                )
-            }
+        #If it doesn't have that file or couldn't parse it
+        #Then fall back on trying to check what directories are importable as modules
+        top_level_paths = tuple({
+            PurePosixPath(x.parents[-2] if len(x.parents) >= 2 else x)
+            for x in package_files
+        })
 
-            #TODO: Any other/better methods for determining importable names?
-            #This seems to produce a fair amount of correct values, but also a fair number of duplicates across packages
+        #Check for any top-level python files, as these should also be importable
+        importable_files = {
+            file.stem
+            for file in top_level_paths
+            if file.name.endswith('.py')
+            and not file.name.startswith('_')
+        }
 
-            #Filter out any empty strings as this will mess up later queries
-            return list(x for x in importable_files | importable_dirs if x) or None
+        #Check for any top-level paths that contain an __init__.py
+        importable_dirs = {
+            directory.name
+            for directory in (Path(y) for y in top_level_paths)
+            if any(
+                file.name == '__init__.py'
+                and file.parent == directory
+                for file in package_files
+            )
+        }
+
+        importable = set(x for x in importable_files | importable_dirs if x)
+        imports.update(importable)
+        return imports
+
+
+    def _get_file_list(self, wheel_file:ZipFile) -> set[FileDetails]:
+        files = set()
+        for file in wheel_file.namelist():
+            #Needed to change comprehension to loop+add in order to support exception handling
+            with suppress(zipfile.BadZipFile, zlib.error):
+                raw_data = wheel_file.read(file)
+                files.add(FileDetails(
+                    file=PurePosixPath(file),
+                    mime_type=magic.from_buffer(raw_data, mime=True),
+                    magic_string=magic.from_buffer(raw_data),
+                ))
+        return files
+
+    def get_package_details(self) -> PackageDetails|None:
+        """Scrapes relevant details about the package to save in the database
+
+        Downloads the .whl (wheel) file for the package and uses contained information to determine how the package is imported
+        This may differ from the name of the package itself
+        eg: BeautifulSoup4 is installed as "pip install beautifulsoup4" but used in source code as "import bs4"
+
+        Additionally, records all the files that the package contains
+
+        :return: A list of names that can be imported from the package
+                 If there are no usable releases of the package, None is returned instead
+        """
+        wheel_files = more_itertools.peekable(self._get_wheel_files())
+        try:
+            wheel_files.peek()
+        except StopIteration:
+            return None
+
+        package_info = self.get_package_info()
+        package_details = PackageDetails(name=self.package_name, serial=package_info['last_serial'])
+        for wheel_file in wheel_files:
+            with wheel_file as wheel_data:
+                package_details.imports.update(self._get_imports(wheel_data))
+                package_details.files.update(self._get_file_list(wheel_data))
+        return package_details
 
     @staticmethod
-    def _web_request(url:str, *, retries:int=5) -> requests.Response:
+    def _web_request(url:str, *, retries:int=5, **kwargs) -> requests.Response:
         """Attempts to retrieve the web content from the specified URL
 
         From a software design perspective, this doesn't necessarily belong in this class
@@ -352,7 +465,7 @@ class PyPIPackage:
         """
         for _ in range(retries+1):
             try:
-                web_request = requests.get(url)
+                web_request = requests.get(url, **kwargs)
 
                 match web_request.status_code:
                     case HTTPStatus.OK:
@@ -373,6 +486,7 @@ class PyPIPackage:
 
         raise requests.exceptions.RequestException('Could not get requested data')
 
+    #============================== Class Attributes ==============================#
     _API_PACKAGE_URL:ClassVar[str] = 'https://pypi.org/pypi/{package_name}/json'
 
 
@@ -421,7 +535,7 @@ def main():
 
         #Break into chunks to process
         CHUNK_SIZE:Final[int] = 500
-        chunked_entries = chunked(to_process.items(), CHUNK_SIZE) #Process 500 at a time to speed up
+        chunked_entries = chunked(to_process.keys(), CHUNK_SIZE) #Process 500 at a time to speed up
         chunk_count = int(math.ceil(len(to_process) / CHUNK_SIZE))
 
         progress_iter = tqdm(
@@ -435,8 +549,8 @@ def main():
         for chunk in progress_iter:
             with concurrent.futures.ThreadPoolExecutor() as pool:
                 threads = [
-                    pool.submit(PyPIPackage(name).get_imports)
-                    for name, serial in chunk
+                    pool.submit(PyPIPackage(name).get_package_details)
+                    for name in chunk
                 ]
 
                 progress_bar = tqdm(
@@ -447,15 +561,13 @@ def main():
                     position=None, leave=None,
                     disable=not to_process,
                 )
-                list(progress_bar)
 
-                for (package_name, last_serial), future in zip(chunk, threads):
+                for future in progress_bar:
                     with suppress(requests.exceptions.ConnectionError, requests.exceptions.RequestException):
-                        import_names = future.result()
-                        #If we can't figure out the import names, skip the package so we don't end up with empty values
-                        if not import_names:
+                        package_details = future.result()
+                        if not package_details:
                             continue
-                        db.add_package_imports(package_name, import_names, serial=last_serial)
+                        db.add_package(package_details)
 
 if __name__ == '__main__':
     main()
